a4fbb25dcb145c435ba9e47053c3a47113dfd34a
Jules <31597717+julesteeuwen@users.noreply.github.com>
2024-03-11 11:06:32 +0100

    update

--- EWMA.py
+++ EWMA.py
@@ -9,12 +9,14 @@ def SMA(data, period):
     # Split the data into timeframes with period datapoints.
 
     # Calculate the average for each timeframe
-
+    return
 
 
 
 
 # Weighted Moving Average (WMA)
+    
+
 
 # Exponentially Weighted Moving Average (EWMA)
     
