2dc166dc87cad04b5d29488db701dac538647c0e
David Hasler <david.j.hasler@gmail.com>
2024-03-11 10:53:02 +0100

    asdad

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def read_data(filename):
     """
@@ -6,4 +7,11 @@ def read_data(filename):
     """
 
     
-    return
+    return dataframe
+
+def sort_data(df):
+    """
+    Takes the dataframe and sorts it by ANSP, returns data as the sorted dataframe
+    """
+    SortedData = df.sort_values(['ENTITY_NAME','FLT_DATE'])
+    return SortedData
