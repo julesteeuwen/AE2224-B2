cd29492cc2daa48992d85c41b7d1b338e538f096
Jules <31597717+julesteeuwen@users.noreply.github.com>
2024-03-11 11:21:36 +0100

    udpate

--- EWMA.py
+++ EWMA.py
@@ -5,15 +5,16 @@ import numpy as np
 # Simple Moving Average (SMA)
 df = pd.read_csv('Datasets/AAPL.csv')
 df = df[['Date', 'Close']]
-print(df)
 
-def SMA(data, period):
+def SMA(df, period):
     
     # Split the data into timeframes with period datapoints.
-    
+    df['MA'] = df[['Close']].rolling(period).mean()
 
     # Calculate the average for each timeframe
-    return
+    return df
+
+print(SMA(df, 5))
 
 
 
