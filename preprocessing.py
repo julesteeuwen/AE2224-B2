798e451e630e2885772480679fd0772f5496e473
StijnCuppers <160853699+StijnCuppers@users.noreply.github.com>
2024-03-14 10:32:54 +0100

    update

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import numpy as np
 import statsmodels as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def read_data(filename):
     """
     Reads all of the data from the file, returns the data as a dataframe
     """
     dataframe = pd.read_csv(filename)
+    dataframe = dataframe.dropna()
     return dataframe
 
 def sort_data(dataframe):
@@ -50,9 +53,32 @@ def get_data(ANSPName, ANSPsdf, ANSPs):
         ANSPIndex = ANSPs.index(ANSPName)
     return ANSPsdf[ANSPIndex]
 
-def Fuller_test():
-    read_data()
-    pd.DataFrame["Month"] = pd.to_datetime(pd.DataFrame["Month"])
+def Fuller_test(dataframe, parameter):
+    """
+    Checks if the parameter is stationary, returns True of stationary, False if not
+    """
+    dataframe["FLT_Date"] = pd.to_datetime(dataframe["FLT_DATE"], format=r"%d-%m-%Y")   
+    dataframe.index = dataframe["FLT_Date"]
+    rolling_mean = dataframe[parameter].rolling(7).mean()
+    print(rolling_mean)
+    rolling_std = dataframe[parameter].rolling(7).std()
+    error_mean = 0
+    error_std = 0
+    for i in range(12,len(dataframe[parameter])):
+        error_mean += (dataframe[parameter][i] - rolling_mean[i])
+        error_std += (dataframe[parameter][i] - rolling_std[i])
+    print(f"mean error = {error_mean/len(dataframe[parameter])}, std error = {error_std/len(dataframe[parameter])}")
 
-dataframe = read_data('Datasets/AAPL.csv')
-print(dataframe.head())
+
+    plt.plot(np.array(dataframe["FLT_Date"].values), np.array(dataframe[parameter].values), label='Data', color = "red")
+    plt.plot(rolling_mean, color = "blue", label = "Rolling Mean")
+    plt.plot(rolling_std, color = "green", label = "Rolling Std")
+    plt.legend(loc = "best")
+    plt.draw()
+    plt.show()
+
+dataframe = read_data('Datasets/split_2014-2016.csv')
+dataframe = dataframe[dataframe['ENTITY_NAME'] == "ENAV"]
+Fuller_test(dataframe, "CPLX_FLIGHT_HRS")
+
+
