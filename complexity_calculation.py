17fb58236c3e919920dbd8db95be72be05e864b0
Matyas Karasek <kmatyas65@gmail.com>
2024-03-14 11:48:08 +0100

    idk

---
+++ complexity_calculation.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+
+def calculate_scores_daily(data):
+    #Calculate individual interaction scores and complexity score for each day
+
+    #Adjusted density
+    data["Adjusted_density"] = data["CPLX_INTER"] / data["CPLX_FLIGHT_HRS"] * 60
+
+    #Vertical score
+    data["Vertical_score"] = data["VERTICAL_INTER_HRS"] / data["CPLX_INTER"]
+
+    data["Horizontal_score"] = data["HORIZ_INTER_HRS"] / data["CPLX_INTER"]
+
+    data["Speed_score"] = data["SPEED_INTER_HRS"] / data["CPLX_INTER"]
+
+    data["Complexity_score"] = data["Adjusted_density"] * (data["Vertical_score"] + data["Horizontal_score"] + data["Speed_score"])
+
+    return data
+
+def calculate_scores_monthly(data):
+    #Group by year and month
+    data = data.drop(columns = ['MONTH_MON', 'FLT_DATE', 'ENTITY_TYPE', 'ENTITY_NAME'])
+    group_month = data.groupby(['YEAR', 'MONTH_NUM']).sum()
+
+    
+
+    
+
+    #Calculate scores for given month
+
+    
+    
+    return calculate_scores_daily(group_month)
+
+
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from preprocessing import *
+from complexity_calculation import calculate_scores_daily, calculate_scores_monthly
+
+
+
 TrafficData1 = read_data("Datasets/split_2014-2016.csv")
 SortedData = sort_data(TrafficData1)
+
 ANSPs = ANSPs(SortedData)
 ANSPs = cleanlist(ANSPs)
 ANSPsdf = split_data(SortedData, ANSPs)
 ANSPName = input("Which ANSP would you want data from?")
 data = get_data(ANSPName, ANSPsdf, ANSPs)
+#data = calculate_scores_daily(data)
+data = calculate_scores_monthly(data)
+
+
+print(data)
+
+
