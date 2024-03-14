6c79d314c73dfd7867f2be98d07957c13ac6ba6e
InesBlanco <inesblancofuentes@gmail.com>
2024-03-14 10:34:01 +0100

    test

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,8 @@ def read_data(filename):
     """
     Reads all of the data from the file, returns the data as a dataframe
     """
+    # Read CSV file into DataFrame df
+    dataframe = pd.read_csv(filename, index_col=0)
 
     
     return dataframe
