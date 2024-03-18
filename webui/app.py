91f4e9c584fecd80b358a16af97818c5052e9a28
Jules <31597717+julesteeuwen@users.noreply.github.com>
2024-03-18 22:24:00 +0100

    test

--- webui/app.py
+++ webui/app.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-df = pd.read_csv('Datasets/split_2014-2016.csv', index_col='FLT_DATE', parse_dates=True, date_format='%d-%m-%Y')
-df.dropna(inplace=True)
+# df = pd.read_csv('Datasets/split_2014-2016.csv', index_col='FLT_DATE', parse_dates=True, date_format='%d-%m-%Y')
+# df.dropna(inplace=True)
 
 @app.route('/')
 def hello():
