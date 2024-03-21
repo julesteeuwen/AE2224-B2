1da18d354032b35b58ce1ab8695ad9b41f157235
Jules <31597717+julesteeuwen@users.noreply.github.com>
2024-03-21 12:29:08 +0100

    update ui

--- webui/pages/index.tsx
+++ webui/pages/index.tsx
@@ -1,22 +1,70 @@
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
+import { Select, SelectItem } from "@nextui-org/react"
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
+import {Tabs, Tab, Card, CardBody, CardHeader} from "@nextui-org/react";
+
+const ansps = [
+	"LVNL",
+	"Skyguide"
+]
+let tabs = [
+    {
+      id: "photos",
+      label: "Visualisation",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+    },
+    {
+      id: "music",
+      label: "Forecasting",
+      content: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    },
+    {
+      id: "videos",
+      label: "Videos",
+      content: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+  ];
 
 export default function IndexPage() {
 	return (
 		<DefaultLayout>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+				
+				<Select
+					label="ANSPs"
+					placeholder="Select the ANSPs"
+					selectionMode="multiple"
+					className="max-w-xs"
+					>
+					{ansps.map((ansp) => (
+						<SelectItem key={ansp} value={ansp}>
+						{ansp}
+						</SelectItem>
+					))}
+				</Select>
+				<Tabs aria-label="Dynamic tabs" items={tabs}>
+					{(item) => (
+					<Tab key={item.id} title={item.label}>
+						<Card>
+						<CardBody>
+							{item.content}
+						</CardBody>
+						</Card>  
+					</Tab>
+					)}
+				</Tabs>
 				<div className="inline-block max-w-lg text-center justify-center">
-					<h1 className={title()}>Make&nbsp;</h1>
-					<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+					<h1 className={title()}>Complexity score analysis and forecasting&nbsp;</h1>
+					<h1 className={title({ color: "violet" })}>AE2224-B2&nbsp;</h1>
 					<br />
 					<h1 className={title()}>
-						websites regardless of your design experience.
+						
 					</h1>
 					<h4 className={subtitle({ class: "mt-4" })}>
 						Beautiful, fast and modern React UI library.
@@ -44,15 +92,8 @@ export default function IndexPage() {
 						GitHub
 					</Link>
 				</div>
-
-				<div className="mt-8">
-					<Snippet hideSymbol hideCopyButton variant="bordered">
-						<span>
-							Get started by editing <Code color="primary">pages/index.tsx</Code>
-						</span>
-					</Snippet>
-				</div>
 			</section>
+
 		</DefaultLayout>
 	);
 }
