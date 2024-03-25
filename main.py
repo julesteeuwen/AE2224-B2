3f61535b1c847573ae68b94a3628067df34627b0
Matyas Karasek <kmatyas65@gmail.com>
2024-03-25 11:52:26 +0100

    kjgy

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from preprocessing import cleanlist,ANSPs,split_data,get_data, read_data
 from display_graphs import displaygraphs, plot_entire_dataset, plot_by_ANSP
 from complexity_calculation import calculate_scores_daily, calculate_scores_monthly, calculate_scores_yearly
+#from EWMA import plot_decompose
 
 
 
@@ -11,9 +12,15 @@ TrafficData1 = read_data("Datasets/split_2017-2019.csv") #Change to choose CSV f
 
 
 
+
+
+
 ANSPs = ANSPs(TrafficData1)
 ANSPs = cleanlist(ANSPs)
 ANSPsdf = split_data(TrafficData1, ANSPs)
+
+
+
 
 #for plotting data selected 'day', 'month' or 'year'
 #plot_entire_dataset(TrafficData1, 'month')
