d7dcbb1ee32ace8f1043106e801f99c646db0052
Jules <31597717+julesteeuwen@users.noreply.github.com>
2024-03-27 00:43:00 +0100

    test

--- webapi/api/app.py
+++ webapi/api/app.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 import sys, os, inspect
 
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-parentparentdir = os.path.dirname(parentdir)
-sys.path.insert(0, parentparentdir) 
+# currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# parentdir = os.path.dirname(currentdir)
+# parentparentdir = os.path.dirname(parentdir)
+# sys.path.insert(0, parentparentdir) 
 
 from preprocessing import get_data, read_data, ANSPs, split_data, cleanlist
 
