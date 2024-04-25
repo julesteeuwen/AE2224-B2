f8db0c2b4b42b6ab38a3371f54a1ace266b8c5e5
David Hasler <david.j.hasler@gmail.com>
2024-04-25 11:35:43 +0200

    main

--- main.py
+++ main.py
@@ -110,3 +110,18 @@ while True:
     # Call the function based on the input
     options.get(input_choice, lambda: print('Invalid choice'))()
 
+
+print(data)
+# data = calculate_scores_daily(data)
+# auto_ARIMA(data,"Complexity_score",plotting=True)
+if mutlipleseason:
+    TrafficData2 = readfilemultipleseason("Datasets/split_2014-2016.csv")
+    ANSPsdf = split_data(TrafficData2, ANSPs)
+    ANSPName = input("Which ANSP would you want data from?")
+    data, ANSPName = get_data(ANSPName, ANSPsdf, ANSPs)
+    df = data[["FLT_DATE", "VERTICAL_INTER_HRS"]]
+    df.insert(0, 'unique_id', 'HRS')
+    df.tail() 
+    df = df.rename(columns={"FLT_DATE":'ds',"VERTICAL_INTER_HRS":'y' })
+    print(df)
+    MultipleSeasonalityDecomp(df, 20)
