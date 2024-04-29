642d596286b2952fa4821036a06fb3da6686f2ed
David Hasler <david.j.hasler@gmail.com>
2024-04-29 09:54:15 +0200

    jhgm

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from AUTO_ARIMA import auto_ARIMA
 
 
 
+
 TrafficData1 = read_data("Datasets/split_2014-2016.csv") #Change to choose CSV file
 
 # Initialise the variables
@@ -15,6 +16,24 @@ selected_period = ''
 ANSPs = ANSPs(TrafficData1)
 ANSPs = cleanlist(ANSPs)
 ANSPsdf = split_data(TrafficData1, ANSPs)
+
+
+print(data)
+# data = calculate_scores_daily(data)
+# auto_ARIMA(data,"Complexity_score",plotting=True)
+if mutlipleseason:
+    TrafficData2 = readfilemultipleseason("Datasets/split_2014-2016.csv")
+    ANSPsdf = split_data(TrafficData2, ANSPs)
+    ANSPName = input("Which ANSP would you want data from?")
+    data, ANSPName = get_data(ANSPName, ANSPsdf, ANSPs)
+    df = data[["FLT_DATE", "VERTICAL_INTER_HRS"]]
+    df.insert(0, 'unique_id', 'HRS')
+    df.tail() 
+    df = df.rename(columns={"FLT_DATE":'ds',"VERTICAL_INTER_HRS":'y' })
+    print(df)
+    MultipleSeasonalityDecomp(df, 20)
+
+
 
 def clear():
     """ Clear the screen and print the welcome message """
@@ -111,17 +130,3 @@ while True:
     options.get(input_choice, lambda: print('Invalid choice'))()
 
 
-print(data)
-# data = calculate_scores_daily(data)
-# auto_ARIMA(data,"Complexity_score",plotting=True)
-if mutlipleseason:
-    TrafficData2 = readfilemultipleseason("Datasets/split_2014-2016.csv")
-    ANSPsdf = split_data(TrafficData2, ANSPs)
-    ANSPName = input("Which ANSP would you want data from?")
-    data, ANSPName = get_data(ANSPName, ANSPsdf, ANSPs)
-    df = data[["FLT_DATE", "VERTICAL_INTER_HRS"]]
-    df.insert(0, 'unique_id', 'HRS')
-    df.tail() 
-    df = df.rename(columns={"FLT_DATE":'ds',"VERTICAL_INTER_HRS":'y' })
-    print(df)
-    MultipleSeasonalityDecomp(df, 20)
