fde0e9ef7a9880eac2ddb4e5911e083e42b17cfc
StijnCuppers <160853699+StijnCuppers@users.noreply.github.com>
2024-05-02 10:39:29 +0200

    test

--- Sensitivity.py
+++ Sensitivity.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 
 #set default parameters
 file = 'Datasets/2017-2019.csv'
@@ -18,7 +19,7 @@ def make_new_DFs(file,sensi_parameter = 0.9):
     dataframe_speed.to_csv('Datasets/2017-2019_sens_speed.csv', sep='\t')
  
 # calculate complexity score based on old dataframes
-def calculate_old_complexity(file, ansps):
+def calculate_actual_complexity(file, ansps):
     df = pd.read_csv(file, index_col= 'FLT_DATE',parse_dates=True, date_format='%d-%m-%Y',delimiter=';').dropna()
     df = df[df['ENTITY_NAME'].isin(ansps)]
     ansps = df['ENTITY_NAME'].unique()
@@ -33,24 +34,50 @@ def calculate_old_complexity(file, ansps):
         cplx.append(cplx_ansp)
     return cplx, ansps
 
-# calculate complexity score based on new dataframes
-def calculate_new_complexity(file, ansps):
-    cplx = 0
-
-    return cplx,  ansps
 
 
 # plot the results
-def plot_old_complexity(file, ansps):
-    cplx, ansps = calculate_old_complexity(file, ansps)
+def plot_achtual_complexity(file, ansps):
+    cplx, ansps = calculate_actual_complexity(file, ansps)
     fig, ax = plt.subplots()
     bar_colors = ['tab:red', 'tab:blue', 'tab:green']
     ax.bar(ansps, cplx, color=bar_colors)
     ax.set_ylabel('Complexity score')
     plt.show()
 
+def plot_change_actual_complexity(file, ansps):
+    cplx_old, ansps = calculate_actual_complexity(file, ansps)
+    cplx_verti, ansps = calculate_actual_complexity('test', ansps)
+    cplx_hori, ansps = calculate_actual_complexity('test', ansps)
+    cplx_speed, ansps = calculate_actual_complexity('test', ansps)
+    change_verti = cplx_old - cplx_verti
+    change_hori = cplx_old - cplx_hori
+    change_speed = cplx_old - cplx_speed
+    
+    x = np.arange(len(ansps))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(layout='constrained')
+
+    for attribute, measurement in penguin_means.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel('Length (mm)')
+    ax.set_title('Impact of interactions on existing complexity score')
+    ax.set_xticks(x + width, species)
+    ax.legend(loc='upper left', ncols=3)
+    ax.set_ylim(0, 250)
+
+    plt.show()
+
+
 def plot_new_complexity(file, ansps):
-    cplx, ansps = calculate_new_complexity(file, ansps)
+    cplx, ansps = calculate_actual_complexity(file, ansps)
     fig, ax = plt.subplots()
     bar_colors = ['tab:red', 'tab:blue', 'tab:green']
     ax.bar(ansps, cplx, color=bar_colors)
@@ -58,3 +85,5 @@ def plot_new_complexity(file, ansps):
     plt.show()
 
 #execute the functions
+#plot_old_complexity(file, ansps)
+make_new_DFs(file)
