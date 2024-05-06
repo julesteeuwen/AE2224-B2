d164b2e11b2f35e99f8fac439c92269f564c99d1
StijnCuppers <160853699+StijnCuppers@users.noreply.github.com>
2024-05-06 10:07:29 +0200

    sensitivity

--- Sensitivity.py
+++ Sensitivity.py
@@ -6,27 +6,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #set default parameters
-file = 'Datasets/2017-2019.csv'
+file = 'Datasets/split_2017-2019.csv'
 ansps = ['Skyguide', 'MUAC', 'DSNA']
 # make new dataframes
-def make_new_DFs(file=file,sensi_parameter = 0.9):
-    dataframe = pd.read_csv(file, index_col= 'FLT_DATE',parse_dates=True, date_format='%d-%m-%Y',delimiter=';').dropna()
+def make_new_DFs(file=file, sensi_parameter = 0.9):
+    dataframe = pd.read_csv(file,parse_dates=True, index_col= 'YEAR', date_format='%d-%m-%Y',delimiter=',').dropna()
     new_df = dataframe.copy()
     new_df['VERTICAL_INTER_HRS'] *= sensi_parameter
-    new_df.to_csv("Datasets/2017-2019_vertical_sensitivity.csv", index=True)
+    new_df.to_csv("Datasets/2017-2019_vertical_sensitivity.csv", index=False)
 
     new_df = dataframe.copy()
     new_df['HORIZ_INTER_HRS'] *= sensi_parameter
-    new_df.to_csv("Datasets/2017-2019_horizontal_sensitivity.csv", index=True)
+    new_df.to_csv("Datasets/2017-2019_horizontal_sensitivity.csv", index=False)
 
     new_df = dataframe.copy()
     new_df['SPEED_INTER_HRS'] *= sensi_parameter
-    new_df.to_csv("Datasets/2017-2019_speed_sensitivity.csv", index=True)
-make_new_DFs(file,0.9)
+    new_df.to_csv("Datasets/2017-2019_speed_sensitivity.csv", index=False)
+#make_new_DFs(file,0.9)
 
 # calculate complexity score based on old dataframes
 def calculate_actual_complexity(file, ansps):
-    df = pd.read_csv(file, index_col= 'FLT_DATE',parse_dates=True, date_format='%d-%m-%Y',delimiter=';').dropna()
+    df = pd.read_csv(file, index_col= 'FLT_DATE',parse_dates=True, date_format='%d-%m-%Y',delimiter=',').dropna()
     df = df[df['ENTITY_NAME'].isin(ansps)]
     ansps = df['ENTITY_NAME'].unique()
     cplx = []
@@ -56,48 +56,37 @@ def plot_change_actual_complexity(file, ansps):
     cplx_verti, ansps = calculate_actual_complexity('Datasets/2017-2019_vertical_sensitivity.csv', ansps)
     cplx_hori, ansps = calculate_actual_complexity('Datasets/2017-2019_horizontal_sensitivity.csv', ansps)
     cplx_speed, ansps = calculate_actual_complexity('Datasets/2017-2019_speed_sensitivity.csv', ansps)
-    change_verti = cplx_old - cplx_verti
-    change_hori = cplx_old - cplx_hori
-    change_speed = cplx_old - cplx_speed
+    change_verti = []
+    change_hori = []
+    change_speed = []
+    for i in range(len(cplx_old)):
+        change_verti.append(abs((cplx_verti[i] - cplx_old[i])/cplx_old[i])*100)
+        change_hori.append(abs((cplx_hori[i] - cplx_old[i])/cplx_old[i])*100)
+        change_speed.append(abs((cplx_speed[i] - cplx_old[i])/cplx_old[i])*100)
 
     print(change_verti, change_hori, change_speed)
+    # set width of bar 
+    barWidth = 0.25
+    fig = plt.subplots(figsize =(12, 8)) 
+ 
+    # Set position of bar on X axis 
+    br1 = np.arange(len(change_verti)) 
+    br2 = [x + barWidth for x in br1] 
+    br3 = [x + barWidth for x in br2] 
+ 
+    # Make the plot
+    plt.bar(br1, change_verti, color = 'tab:orange', width = barWidth, edgecolor ='grey', label ='Vertical Interactions') 
+    plt.bar(br2, change_hori, color ='tab:green', width = barWidth, edgecolor ='grey', label ='Horizontal Interactions') 
+    plt.bar(br3, change_speed, color ='tab:blue', width = barWidth, edgecolor ='grey', label ='Speed Interactions') 
+ 
+    # Adding Xticks 
+    plt.ylabel('Complexity score change(%)', fontweight ='bold', fontsize = 15) 
+    plt.xticks([r + barWidth for r in range(len(change_verti))], ansps)
+ 
+    plt.legend()
+    plt.show() 
 
-    ''''
-    penguin_means = {
-    'Bill Depth': (18.35, 18.43, 14.98),
-    'Bill Length': (38.79, 48.83, 47.50),
-    'Flipper Length': (189.95, 195.82, 217.19),
-}
-    x = np.arange(len(ansps))  # the label locations
-    width = 0.25  # the width of the bars
-    multiplier = 0
-
-    fig, ax = plt.subplots(layout='constrained')
-
-    for attribute, measurement in penguin_means.items():
-        offset = width * multiplier
-        rects = ax.bar(x + offset, measurement, width, label=attribute)
-        ax.bar_label(rects, padding=3)
-        multiplier += 1
-
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_ylabel('Length (mm)')
-    ax.set_title('Impact of interactions on existing complexity score')
-    ax.set_xticks(x + width, species)
-    ax.legend(loc='upper left', ncols=3)
-    ax.set_ylim(0, 250)
-
-    plt.show()
-    '''
-
-
-def plot_new_complexity(file, ansps):
-    cplx, ansps = calculate_actual_complexity(file, ansps)
-    fig, ax = plt.subplots()
-    bar_colors = ['tab:red', 'tab:blue', 'tab:green']
-    ax.bar(ansps, cplx, color=bar_colors)
-    ax.set_ylabel('Complexity score')
-    plt.show()
+    
 
 #execute the functions
 plot_change_actual_complexity(file, ansps)
