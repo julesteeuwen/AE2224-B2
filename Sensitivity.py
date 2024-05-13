fff2bd5901b300e159ba9e85630599240390518f
StijnCuppers <160853699+StijnCuppers@users.noreply.github.com>
2024-05-13 14:13:37 +0200

    hey

--- Sensitivity.py
+++ Sensitivity.py
@@ -63,16 +63,27 @@ def calculate_actual_complexity(file, ansps, plotting = True):
 
     return cplx, ansps
 
-def calculate_predicted_complexity(ansps, n ):
-    test = predict_ansp_TBATS('Skyguide', 'HORIZ_INTER_HRS' ,n)
-    print(test)
+def calculate_predicted_complexity(ansps, n):
+    for ansp in ansps:
+        Horizontal_lst = predict_ansp_TBATS(ansp, 'HORIZ_INTER_HRS' ,n)*0.9
+        Vertical_lst = predict_ansp_TBATS(ansp, 'VERTICAL_INTER_HRS' ,n)*0.9
+        Speed_lst = predict_ansp_TBATS(ansp, 'SPEED_INTER_HRS' ,n)*0.9
+        Horizontal = sum(Horizontal_lst.values)
+        Vertical = sum(Vertical_lst.values)
+        Speed = sum(Speed_lst.values)
+    print(Horizontal)
+    cplx_verti = 0
+    cplx_hori = 0
+    cplx_speed = 0
+    return cplx_verti, cplx_hori, cplx_speed, ansps
+calculate_predicted_complexity(ansps, n)
 
 
 
 
 def plot_change_actual_complexity(file, ansps):
     cplx_old, ansps = calculate_actual_complexity(file, ansps)
-    cplx_verti, ansps = calculate_actual_complexity('Datasets/2017-2019_vertical_sensitivity.csv', ansps)
+    cplx_verti, cplx_hori, cplx_speed, ansps = calculate_actual_complexity('Datasets/2017-2019_vertical_sensitivity.csv', ansps)
     cplx_hori, ansps = calculate_actual_complexity('Datasets/2017-2019_horizontal_sensitivity.csv', ansps)
     cplx_speed, ansps = calculate_actual_complexity('Datasets/2017-2019_speed_sensitivity.csv', ansps)
     change_verti = []
@@ -110,7 +121,50 @@ def plot_change_actual_complexity(file, ansps):
     #plt.legend()
     plt.show() 
 
+def plot_change_predicted_complexity(file, ansps, n):
+    cplx_old, ansps = calculate_actual_complexity(file, ansps)
+    cplx_verti, cplx_hori, cplx_speed, ansps = calculate_predicted_complexity(ansps,n)
+    change_verti = []
+    change_hori = []
+    change_speed = []
+    for i in range(len(cplx_old)):
+        change_verti.append(abs((cplx_verti[i] - cplx_old[i])/cplx_old[i])*100)
+        change_hori.append(abs((cplx_hori[i] - cplx_old[i])/cplx_old[i])*100)
+        change_speed.append(abs((cplx_speed[i] - cplx_old[i])/cplx_old[i])*100)
+
+    print(change_verti, change_hori, change_speed)
+    # set width of bar 
+    barWidth = 0.25
+    fig = plt.subplots(figsize =(12, 8)) 
+ 
+    # Set position of bar on X axis 
+    br1 = np.arange(len(change_verti)) 
+    br2 = [x + barWidth for x in br1] 
+    br3 = [x + barWidth for x in br2] 
+ 
+    # Make the plot
+    plt.bar(br1, change_verti, color = 'tab:orange', width = barWidth, edgecolor ='grey', label ='Vertical Interactions') 
+    plt.bar(br2, change_hori, color ='tab:green', width = barWidth, edgecolor ='grey', label ='Horizontal Interactions') 
+    plt.bar(br3, change_speed, color ='tab:blue', width = barWidth, edgecolor ='grey', label ='Speed Interactions') 
+ 
+    # Adding Xticks 
+    plt.ylabel('Complexity score change(%)', fontweight ='bold', fontsize = 15) 
+    plt.xticks([r + barWidth for r in range(len(change_verti))], ansps)
+ 
+    legend = plt.legend(loc='lower center', shadow=True, fancybox=True,bbox_to_anchor=(0.5, -0.3))
+
+    #plt.setp(legend.get_title(), fontsize=30)
+    plt.setp(legend.get_texts(), fontsize=25)
+    plt.tight_layout()
+    #plt.legend()
+    plt.show() 
+
+    
+
+
     
 
 #execute the functions
-calculate_predicted_complexity('skyguide',n)
+#plot_change_predicted_complexity(file, ansps, n)
+#cplx_verti, ansps = calculate_actual_complexity('Datasets/2017-2019_vertical_sensitivity.csv', ansps)
+#print(cplx_verti)
